82f209022174990a13f60a70c4eb161b3a565974
Andreea <andreea.stoenescu@hotmail.com>
2023-03-07 07:01:31 +0000

    add links to the nav bar using the React Link component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,16 @@
 import { Fragment } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const Navigation = () => {
     return (
       <Fragment>
-        <div>
-          <h1>I am the navbar</h1>
+        <div className="navigation">
+          <Link className="logo-container" to="/">
+            <div>Logo</div>
+          </Link>
+          <div className="nav-links-container">
+            <Link className="nav-link" to="/shop">SHOP</Link>
+          </div>
         </div>
         <Outlet />
       </Fragment>
