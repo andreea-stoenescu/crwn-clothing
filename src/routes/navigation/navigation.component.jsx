9dd47deebc54404822084ea7a2099a760e46eb9e
Andreea <andreea.stoenescu@hotmail.com>
2023-03-07 06:53:41 +0000

    use a Fragment as the root tag, to not actually render anything other than its contents

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,14 @@
+import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 
 const Navigation = () => {
     return (
-      <div>
+      <Fragment>
         <div>
           <h1>I am the navbar</h1>
         </div>
         <Outlet />
-      </div>
+      </Fragment>
     )
 }
 
