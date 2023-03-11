2c66c84e249b938a2b3fbbd2dd2e871472f4087b
Andreea <andreea.stoenescu@hotmail.com>
2023-03-11 18:31:27 +0000

    code formatting fix- navigation component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,20 +4,24 @@ import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
-    return (
-      <Fragment>
-        <div className="navigation">
-          <Link className="logo-container" to="/">
-            <CrwnLogo className="logo" />
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
           </Link>
-          <div className="nav-links-container">
-            <Link className="nav-link" to="/shop">SHOP</Link>
-            <Link className="nav-link" to="/auth">SIGN IN</Link>
-          </div>
+          <Link className="nav-link" to="/auth">
+            SIGN IN
+          </Link>
         </div>
-        <Outlet />
-      </Fragment>
-    )
-}
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
 
 export default Navigation;
