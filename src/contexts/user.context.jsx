2352461a6f7880d5c78fa0c6e944852a1ba917da
Andreea <andreea.stoenescu@hotmail.com>
2023-03-13 07:00:37 +0000

    centralise the authentication code

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,16 +1,30 @@
-import { createContext, useState } from "react";
-
+import { createContext, useState, useEffect } from "react";
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
-    currentUser: null,
-    setCurrentUser: () => {},
+  currentUser: null,
+  setCurrentUser: () => {},
 });
 
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const value = {
-        currentUser,
-        setCurrentUser
-    };
-    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+  const [currentUser, setCurrentUser] = useState(null);
+  const value = {
+    currentUser,
+    setCurrentUser,
+  };
+  //because the second param is an empty array, this effect will only run once
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -8,11 +8,7 @@ import { signOutUser } from "../../utils/firebase/firebase.utils";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
-  const signOutHandler = async () => {
-    await signOutUser();
-    setCurrentUser(null);
-  };
+  const { currentUser } = useContext(UserContext);
   return (
     <Fragment>
       <div className="navigation">
@@ -25,7 +21,7 @@ const Navigation = () => {
           </Link>
           <Fragment>
             {currentUser ? (
-              <span className="nav-link" onClick={signOutHandler}>
+              <span className="nav-link" onClick={signOutUser}>
                 SIGN OUT
               </span>
             ) : (
